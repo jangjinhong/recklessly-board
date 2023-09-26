5af503dedd801125737f4914f19a8212a497e317
jangjinhong <honge1122@naver.com>
2023-09-27 00:17:02 +0900

    2023-09-26

--- src/main/java/com/recklesslystudy/board/controller/BoardController.java
+++ src/main/java/com/recklesslystudy/board/controller/BoardController.java
@@ -2,13 +2,21 @@ package com.recklesslystudy.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
-    @GetMapping("/")        // ("/")로 들어갔을때
-    @ResponseBody           // return값을 띄워줄 어노테이션
-    public String main() {
-        return "hello world";
+
+    @GetMapping("/board/write") // localhost:8080/board/write
+    public String boardWriteForm() {
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content) {
+        System.out.println("제목: " + title);
+        System.out.println("내용: " + content);
+        return "";
     }
 }
