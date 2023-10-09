69d86502a01ee9ac0019705262b10f29499795d1
jangjinhong <honge1122@naver.com>
2023-10-09 21:45:45 +0900

    2023-10-09

--- src/main/java/com/recklesslystudy/board/controller/BoardController.java
+++ src/main/java/com/recklesslystudy/board/controller/BoardController.java
@@ -1,13 +1,19 @@
 package com.recklesslystudy.board.controller;
 
 import com.recklesslystudy.board.entity.Board;
+import com.recklesslystudy.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/write") // localhost:8080/board/write
     public String boardWriteForm() {
@@ -16,7 +22,22 @@ public class BoardController {
 
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board) {
-        System.out.println(board.getTitle());
+        boardService.write(board);
         return "";
     }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardList";     //view
+    }
+
+    @GetMapping("board/view")
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardView";
+    }
 }
+
+
--- src/main/java/com/recklesslystudy/board/service/BoardService.java
+++ src/main/java/com/recklesslystudy/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.recklesslystudy.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired
@@ -12,4 +14,14 @@ public class BoardService {
     public void write(Board board) {
         boardRepository.save(board);
     }
+
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
 }
